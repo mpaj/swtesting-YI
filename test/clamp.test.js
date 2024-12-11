94012a6aed36a0d8db7c30483036ad44efbc202a
Eetu Kallio <eetu.kallio@icloud.com>
2024-12-11 20:59:30 +0200

    Remove some redundant test cases from clamp.test.js and uncomment failing tests.

--- test/clamp.test.js
+++ test/clamp.test.js
@@ -2,30 +2,20 @@ import clamp from '../COMP.SE.200-2024-2025-1/src/clamp.js';
 import { expect } from 'chai';
 
 describe('clamp', function() {
-    // it('should clamp values within the inclusive lower and upper bounds', function() {
-    //     expect(clamp(-10, -5, 5)).to.equal(-5)
-    //     expect(clamp(10, -5, 5)).to.equal(5)
-    // })
+    it('should clamp values within the inclusive lower and upper bounds', function() {
+        expect(clamp(-10, -5, 6)).to.equal(-5)
+        expect(clamp(10, -5, 6)).to.equal(6)
+    })
     
     it('should work with a `lower` greater than `upper`', function() {
         expect(clamp(10, 5, -5)).to.equal(5)
         expect(clamp(-10, 5, -5)).to.equal(-5)
     })
     
-    it('should clamp negative numbers', function() {
-        expect(clamp(-10, -5, 5)).to.equal(-5)
-        expect(clamp(-10, 5, -5)).to.equal(-5)
+    it('should not alter zero in any way', function() {
+        expect(clamp(0, -5, 5)).to.equal(0)
+        expect(clamp(0, 5, -5)).to.equal(0)
     })
-    
-    // it('should clamp positive numbers', function() {
-    //     expect(clamp(10, -5, 5)).to.equal(5)
-    //     expect(clamp(10, 5, -5)).to.equal(5)
-    // })
-    
-    // it('should not alter zero in any way', function() {
-    //     expect(clamp(0, -5, 5)).to.equal(0)
-    //     expect(clamp(0, 5, -5)).to.equal(0)
-    // })
     
     it('should clamp to `0` when `lower` and `upper` are `0`', function() {
         expect(clamp(-5, 0, 0)).to.equal(0)
